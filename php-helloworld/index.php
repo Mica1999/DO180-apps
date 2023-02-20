d22558f2313635564f322af2cfdbdc896a988cf4
Student User <student@workstation.lab.example.com>
2023-02-20 05:51:01 -0500

    Update index page content

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Change should trigger automatic re-build and re-deploy?\n"
-print "Hmmmmm. This is a test.\n"
+print "Change should trigger automatic re-build and re-deploy?\n";
+print "Hmmmmm. This is a test.\n";
 ?>
